6b72a3586637080741e5b7a89981b02412c7a52c
bbourgeois <benjamin.bourgeois@zenika.com>
2023-08-21 22:13:46 +0200

    :construction: first function

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -1,7 +1,9 @@
-export interface TgtgClientOptions {
+import { TgtgApiConfig } from './tgtg-endpoints';
+
+export type TgtgClientOptions = {
   /**
- * Email of your TooGoodToGo account.
- */
+   * Email of your TooGoodToGo account.
+   */
   email?: string;
   accessToken?: string;
   refreshToken?: string;
@@ -11,12 +13,33 @@ export interface TgtgClientOptions {
   language?: string;
   accessTokenLifetime?: number;
   deviceType?: string;
-}
+};
 
 export class TgtgClient {
+  email: string | null;
+  accessToken: string | null;
+  refreshToken: string | null;
+  userId: string | null;
+  cookie: string | null;
+  userAgent: string | null;
+  language: string;
+  accessTokenLifetime: number;
+  deviceType: string;
+  constructor(options: TgtgClientOptions) {
+    this.email = options.email || null;
+    this.accessToken = options.accessToken || null;
+    this.refreshToken = options.refreshToken || null;
+    this.userId = options.userId || null;
+    this.cookie = options.cookie || null;
+    this.userAgent = options.userAgent || null;
+    this.language = options.language || 'en-UK';
+    this.accessTokenLifetime = options.accessTokenLifetime || TgtgApiConfig.defaultAccessTokenLifetime;
+    this.deviceType = options.deviceType || 'android';
+  }
 
-  
-constructor(options: TgtgClientOptions) {
-  this?
-}
+  async login() {
+    if (!this.email || (this.accessToken && this.refreshToken && this.userId)) {
+      throw new Error('You must provide at least email or access_token, refresh_token and user_id');
+    }
+  }
 }
--- src/lib/tgtg-endpoints.ts
+++ src/lib/tgtg-endpoints.ts
@@ -1,17 +1,17 @@
-export const TgtgEndpoints = {
-    baseUrl: "https://apptoogoodtogo.com/api",
-    itemEndpoint: "/item/v8/",
-    authByEmail: "/auth/v3/authByEmail",
-    authPolling: "/auth/v3/authByRequestPollingId",
-    signUpByEmail: "/auth/v3/signUpByEmail",
-    refresh: "/auth/v3/token/refresh",
-    defaultApkVersion: "22.5.5",
-    userAgents: [
-        "TGTG/{} Dalvik/2.1.0 (Linux; U; Android 9; Nexus 5 Build/M4B30Z)",
-        "TGTG/{} Dalvik/2.1.0 (Linux; U; Android 10; SM-G935F Build/NRD90M)",
-        "TGTG/{} Dalvik/2.1.0 (Linux; Android 12; SM-G920V Build/MMB29K)",
-    ],
-    defaultAccessTokenLifetime: 3600 * 4, // 4 hours
-    pollingWaitTime: 5, // seconds
-    maxPollingTries: 24 // 24 * polling_wait_time = 2 minutes
-}
+export const TgtgApiConfig = {
+  baseUrl: 'https://apptoogoodtogo.com/api',
+  itemEndpoint: '/item/v8/',
+  authByEmail: '/auth/v3/authByEmail',
+  authPolling: '/auth/v3/authByRequestPollingId',
+  signUpByEmail: '/auth/v3/signUpByEmail',
+  refresh: '/auth/v3/token/refresh',
+  defaultApkVersion: '22.5.5',
+  userAgents: [
+    'TGTG/{} Dalvik/2.1.0 (Linux; U; Android 9; Nexus 5 Build/M4B30Z)',
+    'TGTG/{} Dalvik/2.1.0 (Linux; U; Android 10; SM-G935F Build/NRD90M)',
+    'TGTG/{} Dalvik/2.1.0 (Linux; Android 12; SM-G920V Build/MMB29K)',
+  ],
+  defaultAccessTokenLifetime: 3600 * 4, // 4 hours
+  pollingWaitTime: 5, // seconds
+  maxPollingTries: 24, // 24 * polling_wait_time = 2 minutes
+};

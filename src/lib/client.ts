4b7fd4fc919346085a3498f050af20bd871edf82
Benjamin Bourgeois <benjishift@gmail.com>
2023-08-21 17:37:09 +0000

    :construction: work on login

---
+++ src/lib/client.ts
@@ -0,0 +1,22 @@
+export interface TgtgClientOptions {
+  /**
+ * Email of your TooGoodToGo account.
+ */
+  email?: string;
+  accessToken?: string;
+  refreshToken?: string;
+  userId?: string;
+  cookie?: string;
+  userAgent?: string;
+  language?: string;
+  accessTokenLifetime?: number;
+  deviceType?: string;
+}
+
+export class TgtgClient {
+
+  
+constructor(options: TgtgClientOptions) {
+  this?
+}
+}

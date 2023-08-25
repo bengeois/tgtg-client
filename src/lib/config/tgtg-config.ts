ac05c2877a5f1ce758d425a53940dacbb85298aa
bbourgeois <benjamin.bourgeois@zenika.com>
2023-08-25 14:54:39 +0200

    :fire: removing unused code

--- src/lib/config/tgtg-config.ts
+++ src/lib/config/tgtg-config.ts
@@ -2,11 +2,7 @@ export const TgtgConfig = {
   baseUrl: 'https://apptoogoodtogo.com/api',
   authByEmail: 'auth/v3/authByEmail',
   authPolling: 'auth/v3/authByRequestPollingId',
-  items: 'item/v8/',
   refresh: 'auth/v3/token/refresh',
-  defaultApkVersion: '22.5.5',
+  items: 'item/v8/',
   userAgent: 'TooGoodToGo/21.9.0 (813) (iPhone/iPhone 7 (GSM); iOS 15.1; Scale/2.00)',
-  defaultAccessTokenLifetime: 3600 * 4, // 4 hours
-  pollingWaitTime: 5, // seconds
-  maxPollingTries: 24, // 24 * polling_wait_time = 2 minutes
 };
